1b5d2a3377ae6d6a0182a2f585df0ed4207fd4ba
Juhi Maharjan <juhi.maharzan@gmail.com>
2024-11-03 15:30:20 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -4,9 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const navBar = document.querySelector('.nav-bar');
     
     // Skip animation expect index.html
-    console.log("Current pathname:", window.location.pathname);
-    if (window.location.pathname !== '/index.html' && window.location.pathname !== '/') {
-        console.log("Not index.html - applying sticky classes directly");
+    if (window.location.pathname === '/audit.html' || window.location.pathname === '/banking.html' || window.location.pathname === '/education.html' || window.location.pathname === 'it.html' || window.location.pathname === '/entrepreneur.html' || window.location.pathname === '/tax.html' || window.location.pathname === '/portfolio.html' || window.location.pathname === '/elevator.html') {
         // Skip animation
         logoContainer.classList.add('sticky');
         logo.classList.add('shrunk-logo');
